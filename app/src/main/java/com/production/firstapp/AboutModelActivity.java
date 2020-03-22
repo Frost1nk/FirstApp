348587a4520d0877c91587247183b77fab99fae5
Frost1nk <niyozov.daler@yandex.ru>
2020-03-22 17:11:02 +0300

    check

--- app/src/main/java/com/production/firstapp/AboutModelActivity.java
+++ app/src/main/java/com/production/firstapp/AboutModelActivity.java
@@ -1,6 +1,9 @@
 package com.production.firstapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +15,33 @@ public class AboutModelActivity extends AppCompatActivity {
 
     private Model model;
 
+    private TextView labelName;
+    private TextView labelShow;
+    private TextView labelDownload;
+    private TextView labelFavorite;
+
+    private ImageView imagePreview;
+
+    private Button btnSaveGallery;
+    private Button btnSaveMCPE;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_model);
 
-        SerializableJson serializableJson = getIntent().getParcelableExtra("file");
-        if(serializableJson == null){
+        SerializableJson serializableJson = getIntent().getParcelableExtra("json");
+        if(serializableJson == null)
             finish();
             model = new Model(serializableJson.getJsonObject());
-        }
+            labelName = findViewById(R.id.labelName);
+            labelShow = findViewById(R.id.labelShow);
+            labelDownload = findViewById(R.id.labelDownload);
+            labelFavorite = findViewById(R.id.labelFavorite);
+
+            imagePreview = findViewById(R.id.imagePreview);
+
+            btnSaveGallery = findViewById(R.id.btnSaveGallery);
+            btnSaveMCPE = findViewById(R.id.btnSaveMinecraft);
     }
 }
